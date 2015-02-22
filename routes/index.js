a61ba4f3788367b999730276f55ea394e26128f9
Dolly <dollyg@INDOLLYG.corporate.thoughtworks.com>
2015-02-22 18:33:26 +0530

    create game implemented using local json objects and registered user. create game is only available for first user and paly option will be available for next users.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var users = [];
+var gameDetails = {};
+
 router.get("/game",function(req,res){
 	res.render("game");
 })
+
+router.get("/waiting",function(req,res){
+	res.render("waiting");
+})
+
+router.post("/registerUser",function(req,res){
+	var username = req.body.name;
+	var isCreator;
+	if(users.length==0)
+		isCreator = true;
+	users.push(username);
+	console.log(users);
+	res.render("dashboard",{username:username,isCreator:isCreator});
+})
+
+router.get("/createGame",function(req,res){
+	res.render("createGame");
+})
+
+router.post("/createGame",function(req,res){
+	gameDetails.numberOfPlayers = req.body.numberOfPlayers;
+	gameDetails.time = req.body.time;
+	res.redirect("/waiting");
+})
+
 
 module.exports = router;

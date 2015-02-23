f146e4f7bf11e8ebbb9b1087fdc5fc439b84cff2
Kittu Jain <cskratijain@gmail.com>
2015-02-23 08:20:44 +0530

    Created common layout

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -7,14 +7,19 @@ var sendWordToServer = function(){
 }
 
 var broadcastNewWord = function(newWord){
-	var newWordHTML = "<div>"+newWord.newWord+"</div>";
+	var newWordHTML = "<div>"+'<a href="" id = "word">'+newWord.newWord+"</a>"+"</div>";
 	var previousWords = $('#div_words').html();
 	$('#div_words').html(previousWords + " " + newWordHTML);
 }
 
 var onPageLoad = function(){
 	$('#btn_send').click(sendWordToServer);
+	$('#word').click(showMeaning);
 	socket.on('newWord',broadcastNewWord);
+}
+
+var showMeaning = function() {
+	$('#wordMeaning').html("Hello World");
 }
 
 $(onPageLoad);

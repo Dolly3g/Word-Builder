abe315fdaa52ebd11640842d62b9a4f847fd0bcd
Dolly <dollyg@INDOLLYG.corporate.thoughtworks.com>
2015-02-23 18:38:41 +0530

    one by one players can come and say the word. chance of players comes according to their time of joining.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,16 +5,18 @@ var sendWordToServer = function(){
 	var currentUser = $('#hidden_currentUser').val();
 	var newWord = $('#input_word').val();
 	$('#input_word').val("");
-	var username = $('#hidden_username').val();
-	socket.emit('newWord',{newWord:newWord,username:username,users:users,currentUser:currentUser});
+	var ownerOfWord = $('#hidden_username').val();
+	socket.emit('newWord',{newWord:newWord,ownerOfWord:ownerOfWord,users:users,currentUser:currentUser});
 }
 
 var broadcastNewWord = function(data){
-	console.log("data=",data);
-	if(data.currentUser != data.username){
-		$('#input_word').prop('disabled',true);
-	}
-	var newWordHTML = "<div>" + data.username + " : <a href=''>" + data.newWord + "</a></div>";
+	var isInputBoxDisabled;
+	var username = $('#hidden_username').val();
+	$('#hidden_currentUser').val(data.currentUser);
+	isInputBoxDisabled = (data.currentUser != username) ? true : false;
+	$('#input_word').prop('disabled',isInputBoxDisabled);
+
+	var newWordHTML = "<div>" + data.ownerOfWord + " : <a href=''>" + data.newWord + "</a></div>";
 	var previousWords = $('#div_words').html();
 	$('#div_words').html(previousWords + " " + newWordHTML);
 }

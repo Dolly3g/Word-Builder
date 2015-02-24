0732b9a9722df08c6640bba837b44f1a2de662c8
Dolly <dollyg@INDOLLYG.corporate.thoughtworks.com>
2015-02-24 19:04:08 +0530

    username changed to ownerofword in main.js word_div

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,5 @@
 var socket = io();
-var WORD_DIV = "<p style=display:inline-block;>USERNAME: </p> <u><p style=cursor:pointer;color:blue;display:inline-block;text-decoration:underline; class = words onclick = displayWordMeaning('NEWWORD') id='NEWWORD'>NEWWORD</p><br/>";
+var WORD_DIV = "<p style=display:inline-block;>OWNEROFWORD: </p> <u><p style=cursor:pointer;color:blue;display:inline-block;text-decoration:underline; class = words onclick = displayWordMeaning('NEWWORD') id='NEWWORD'>NEWWORD</p><br/>";
 var DIC_URL = "https://api.pearson.com/v2/dictionaries/ldoce5/entries?headword=WORD&apikey=A8x5Zdl19xkxlgaUuErOQc9aufyv5WEH"; 
 
 var sendWordToServer = function(){
